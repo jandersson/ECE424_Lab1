333b581a1f5f68f05b8c9f9a2018811d691c5d89
Jonas Andersson <jonas123@gmail.com>
2014-10-10 21:47:10 -0500

    Update to conform to PEP. Create Login class.

--- Lab1.py
+++ Lab1.py
@@ -1,57 +1,62 @@
 __author__ = 'Jonas'
-from tkinter import * # get widget classes
-from tkinter.messagebox import * # get standard dialogs
 
-def close():
-    root.destroy()
+from Tkinter import *  # get widget classes
 
-def login():
-    ### Write your code here###
-    return
 
-def makeform(root, fields):
-    ### Write your code here###
-    return
+class Login:
 
-def logout():
-    ### Write your code here###
-    return
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
 
-def save():
-    ### Write your code here###
-    return
+        self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+        self.button.pack(side=LEFT)
 
-def verify(entries,dialog):
-    ### Write your code here###
-    return
+    def login(self):
+        print("Hello!")
 
-def makemenu(win,cond):
-    top = Menu(win) # win=top-level window
-    win.config(menu=top) # set its menu option
-    file = Menu(top)
-    top.add_cascade(label='File', menu=file, underline=0)
-    account = Menu(top)
-    top.add_cascade(label='Account', menu=account, underline=0)
-    if cond==False:
-        file.add_command(label='Save', command=save, underline=0,state='disabled')
-        account.add_command(label='Log-In', command=login, underline=0,state='active')
-        account.add_command(label='Log-Out', command=logout, underline=0,state='disabled')
-    else:
-        file.add_command(label='Save', command=save, underline=0,state='active')
-        account.add_command(label='Log-In', command=login, underline=0,state='disabled')
-        account.add_command(label='Log-Out', command=logout, underline=0,state='active')
 
+    def makeform(self, root, fields):
+        ### Write your code here###
+        pass
+
+
+    def logout(self):
+        ### Write your code here###
+        pass
+
+
+    def save(self):
+        ### Write your code here###
+        pass
+
+
+    def verify(self, entries, dialog):
+        ### Write your code here###
+        pass
+
+#
+# def makemenu(win, cond):
+#     top = Menu(win)  # win=top-level window
+#     win.config(menu=top)  # set its menu option
+#     file = Menu(top)
+#     top.add_cascade(label='File', menu=file, underline=0)
+#     account = Menu(top)
+#     top.add_cascade(label='Account', menu=account, underline=0)
+#     if not cond:
+#         file.add_command(label='Save', command=save, underline=0, state='disabled')
+#         account.add_command(label='Log-In', command=login, underline=0, state='active')
+#         account.add_command(label='Log-Out', command=logout, underline=0, state='disabled')
+#     else:
+#         file.add_command(label='Save', command=save, underline=0, state='active')
+#         account.add_command(label='Log-In', command=login, underline=0, state='disabled')
+#         account.add_command(label='Log-Out', command=logout, underline=0, state='active')
 
 if __name__ == '__main__':
 
-
-    root = Tk() # or Toplevel()
-    root.title('ECE424_Lab1') # set window-mgr info
-    makemenu(root,False) # associate a menu bar
-
-##From Lab
-    widget = Button(root, text='Hello GUI World', command=close)
-    widget.pack()
-
+    root = Tk()  # or Toplevel()
+    lab1 = Login(root)
+    # root.title('ECE424_Lab1')  # set window-mgr info
+    # makemenu(root, False)  # associate a menu bar
     root.mainloop()
 
