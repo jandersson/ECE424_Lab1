75591db06bd82384cc6bf9be0cc08f1e59a9c1f1
Jonas Andersson <jonas123@gmail.com>
2014-10-14 21:08:00 -0500

    Move menubar code into class initializer

--- Lab1.py
+++ Lab1.py
@@ -1,52 +1,58 @@
-__author__ = 'Jonas'
+__author__ = 'Jonas Andersson'
 
 from Tkinter import *  # get widget classes
 
+#Initialize Global State
+logged_in = False
 
 class Login:
 
     def __init__(self, master):
+
+        #Instantiate the frame and display it
         frame = Frame(master)
         frame.pack()
 
-        self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
-        self.button.pack(side=LEFT)
+        #Create options menu bar
+        menu_bar = Menu(master)
+        master.config(menu=menu_bar)
+        file = Menu(menu_bar)
+        menu_bar.add_cascade(label='File', menu=file, underline=0)
+        account = Menu(menu_bar)
+        menu_bar.add_cascade(label='Account', menu=account, underline=0)
 
-    def login(self):
-        print("Hello!")
+        #Populate options (in a logged out state)
+        #Register event handlers
+        file.add_command(label='Save', command=self.save, underline=0, state='disabled')
+        account.add_command(label='Log-In', command=self.login(master), underline=0, state='active')
+        account.add_command(label='Log-Out', command=self.logout, underline=0, state='disabled')
 
+    def login(self, master):
+        #Handler for the login event
+        loginwindow = Toplevel(master)
+        user_entry = Entry(loginwindow)
+        user_entry.pack()
+        loginwindow.wait_window()
 
     def makeform(self, root, fields):
         ### Write your code here###
         pass
 
-
     def logout(self):
         ### Write your code here###
         pass
-
 
     def save(self):
         ### Write your code here###
         pass
 
-
     def verify(self, entries, dialog):
         ### Write your code here###
         pass
 
-#
 # def makemenu(win, cond):
-#     top = Menu(win)  # win=top-level window
-#     win.config(menu=top)  # set its menu option
-#     file = Menu(top)
-#     top.add_cascade(label='File', menu=file, underline=0)
-#     account = Menu(top)
-#     top.add_cascade(label='Account', menu=account, underline=0)
 #     if not cond:
-#         file.add_command(label='Save', command=save, underline=0, state='disabled')
-#         account.add_command(label='Log-In', command=login, underline=0, state='active')
-#         account.add_command(label='Log-Out', command=logout, underline=0, state='disabled')
+#
 #     else:
 #         file.add_command(label='Save', command=save, underline=0, state='active')
 #         account.add_command(label='Log-In', command=login, underline=0, state='disabled')
@@ -59,4 +65,5 @@ if __name__ == '__main__':
     # root.title('ECE424_Lab1')  # set window-mgr info
     # makemenu(root, False)  # associate a menu bar
     root.mainloop()
+    root.destroy()
 
